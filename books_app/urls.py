41baa7cb9935af6a0ece0c07551d2cf1a206e5df
Javlonbek0205 <javlonbekdeveloper7@gmail.com>
2024-12-24 17:48:31 +0500

    Updated urls

--- books_app/urls.py
+++ books_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import (add_to_cart_ajax,
                     BookListView,
                     BookDetailView,
-                    AllBookListView,)
+                    AllBookListView, MyBookListView, BookCreateView)
                     #books_list_view,
                     #books_detail_view,
                     #all_books_list_view,
@@ -12,6 +12,8 @@ urlpatterns = [
      path('add-to-cart/', add_to_cart_ajax, name='add_to_cart_ajax'),
      path('all_books/', AllBookListView.as_view(), name='all_books'),
     #path('all_books/', all_books_list_view, name='all_books'),
+     path('my_books/', MyBookListView.as_view(), name='my_books'),
+     path('book_create/', BookCreateView.as_view(), name='book_create'),
      path('book_detail/<int:pk>/', BookDetailView.as_view(), name='books-detail'),
      #path('book_detail/<int:pk>/', books_detail_view, name='books-detail'),
- ]
+]
