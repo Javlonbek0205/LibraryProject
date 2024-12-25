ec3c7f3720d5d283b822cf430827a485996bea17
Javlonbek0205 <javlonbekdeveloper7@gmail.com>
2024-12-25 16:05:57 +0500

    Added book-update and book-delete

--- books_app/urls.py
+++ books_app/urls.py
@@ -2,18 +2,17 @@ from django.urls import path
 from .views import (add_to_cart_ajax,
                     BookListView,
                     BookDetailView,
-                    AllBookListView, MyBookListView, BookCreateView)
+                    AllBookListView, MyBookListView, BookCreateView, BookUpdateView, BookDeleteView)
                     #books_list_view,
                     #books_detail_view,
                     #all_books_list_view,
 urlpatterns = [
      path('', BookListView.as_view(), name='home'),
-    #path('', books_list_view, name='home'),
      path('add-to-cart/', add_to_cart_ajax, name='add_to_cart_ajax'),
      path('all_books/', AllBookListView.as_view(), name='all_books'),
-    #path('all_books/', all_books_list_view, name='all_books'),
      path('my_books/', MyBookListView.as_view(), name='my_books'),
      path('book_create/', BookCreateView.as_view(), name='book_create'),
      path('book_detail/<int:pk>/', BookDetailView.as_view(), name='books-detail'),
-     #path('book_detail/<int:pk>/', books_detail_view, name='books-detail'),
+     path('book_update/<int:pk>/', BookUpdateView.as_view(), name='book-update'),
+     path('book_delete/<int:pk>/', BookDeleteView.as_view(), name='book-delete'),
 ]
